2c6563b9be8912c055e958419082fa037e05b5f3
gowthamnk <gowthamnk67@gmail.com>
2024-09-04 08:04:17 +0530

    new

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/auth-db")
+  .connect(process.env.MONGO_URL)
     .then(() => {
         console.log("MongoDB Connected...");
     })
